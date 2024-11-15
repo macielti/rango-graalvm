8a6595e9c6333f49de32e52f0bd9d02ca98a2b29
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-15 04:15:20 -0300

    bump common clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :exclusions [amazonica]
 
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [net.clojars.macielti/common-clj "37.71.71"]
+                 [net.clojars.macielti/common-clj "38.71.71"]
                  [net.clojars.macielti/http-client-component "0.1.0"]
                  [io.pedestal/pedestal.service "0.7.2"]
                  [io.pedestal/pedestal.jetty "0.7.2"]
